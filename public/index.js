7bd7fecf38660f1eec37fd38fc8d1dbf94b571fd
Amitash <amitu895@gmail.com>
2022-04-03 13:38:44 +0530

    Added multiple layers

--- public/index.js
+++ public/index.js
@@ -9,19 +9,11 @@ class Node {
 	}
 }
 
-let container = document.getElementById('container');
-let rootBox = document.createElement('div');
-container.appendChild(rootBox);
-rootBox.classList.add('box');
-rootBox.style.display = 'inline-block';
-let root = new Node(rootBox);
-addRight(root, 8);
-
-function addRight(root, leng) {
+function addRight(root, rootLayer, leng) {
 	var tmpRoot = root;
 	for(i = 1; i < leng; i++) {
 		var item = document.createElement('div');
-		container.appendChild(item);
+		rootLayer.appendChild(item);
 		item.classList.add('box');
 		item.style.display = 'inline-block';
 		var newNode = new Node(item);
@@ -31,3 +23,25 @@ function addRight(root, leng) {
 	}
 }
 
+function addLayer() {
+	var newLayer = document.createElement('div');
+	var tmp = document.createElement('div');
+	container.appendChild(newLayer);
+	newLayer.appendChild(tmp);
+
+	tmp.classList.add('box');
+	var newRoot = new Node(tmp);
+	addRight(newRoot, newLayer, 8);
+	return newRoot;
+}
+
+let container = document.getElementById('container');
+// let rootBox = document.createElement('div');
+// let rootLayer = document.createElement('div');
+// container.appendChild(rootLayer);
+// rootLayer.appendChild(rootBox);
+
+// rootBox.classList.add('box');
+// let root = new Node(rootBox);
+// addRight(root, rootLayer, 8);
+let layers = [addLayer(), addLayer(), addLayer()];
